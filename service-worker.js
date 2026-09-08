b062f713d7db8a324328d03e4bfe317c6950bc3b
Ch V Krishna <krishna@gmail.com>
2026-09-08 20:05:45 +0530

    Enable PWA service worker

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,9 @@ const urlsToCache = [
   "./news.html",
   "./news.js",
   "./firebase.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
